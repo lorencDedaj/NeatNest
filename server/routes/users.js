b124a21eacdcb7fcb8104da5018b54a75fff9de4
Lorenc Dedaj <le.dedaj@gmail.com>
2025-08-01 20:20:03 -0400

    implement POST user/jobs GET user/jobs by id & GET users/jobs

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,14 @@
 import express from 'express';
-import { getUsers } from '../controllers/users.js';
+import { getUsers, getUserByID, createUser } from '../controllers/users.js';
 
 const router = express.Router();
-
+//GET all users
 router.get('/', getUsers);
+
+//GET users by id
+router.get('/:id', getUserByID);
+
+// POST user --- create new user
+router.post('/', createUser);
 
 export default router;
